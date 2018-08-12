5a6cdfc90f109023e4b2ed97c743d7fb900b4ee9
João Gabriel <joaogabrieloliveira9@gmail.com>
2018-08-12 00:54:33 -0300

    basic structure,  model definitions and insert function

---
+++ index.js
@@ -0,0 +1,103 @@
+const loki = require('lokijs');
+
+const _types = {
+    ANY:        value => value,
+    INTEGER:    value => parseInt(Number(value)),
+    FLOAT:      value => parseFloat(Number(value)),
+    STRING:     value => value == null ? null : value.toString(),
+    BOOLEAN:    value => Boolean(value),
+    DATE:       value => value instanceof Date ? value : (value == null ? null : new Date(value))
+};
+
+class Jotnar {
+    /**
+     * 
+     * @param {Loki} lokiDB Loki instance
+     */
+    constructor (lokiDB) {
+        this._database = lokiDB;
+
+        this.models = {};
+    }
+
+    /**
+     * Defines a new model
+     * @param {string} name Model's name
+     * @param {object} definition Model's properties definitions
+     * @param {boolean} allowExtraProperties If more properties are passed to
+     * the insert function, they should be ignored (false) or included (true)?
+     */
+    define (name, definition, allowExtraProperties) {
+        let self = this;
+
+        if (this.models[name])
+            throw new Error(`Redefinition of model ${name}`);
+        
+        this.models[name] = {
+            _collection: null,
+            _unique: [],
+            _scheme: {},
+            _defaultObject: {},
+            _nonStrict: allowExtraProperties
+        };
+
+        for (let prop in definition) {
+            if (typeof definition[prop] === 'object') {
+                if (definition[prop].unique) this.models[name]._unique.push(prop);
+                this.models[name]._scheme[prop] = {
+                    parse: definition[prop].type || _types.ANY,
+                    notNull: definition[prop].allowNull === false,
+                };
+                this.models[name]._defaultObject[prop] = typeof definition[prop].defaultValue === 'undefined' ? null : definition[prop].defaultValue;
+            } else {
+                this.models[name]._scheme[prop] = {
+                    parse: definition[prop],
+                    notNull: true
+                };
+                this.models[name]._defaultObject[prop] = null;
+            }
+        }
+
+        this.models[name]._collection = this._database.getCollection(name);
+        if (!this.models[name]._collection) {
+            this.models[name]._collection = this._database.addCollection(name, {
+                unique: this.models[name]._unique
+            });
+        }
+        this.models[name]._collection.uniqueNames = this.models[name]._unique;
+
+        /**
+         * Inserts an object or array of objects in the collection
+         * @param {object|object[]} object Object or array of objects to insert
+         */
+        function insert (object) {
+            let objects = Array.isArray(object) ? object : [object];
+            let listToInsert = [];
+            for (let obj of objects) {
+                let toInsert = JSON.parse(JSON.stringify(self.models[name]._defaultObject));
+                for (let prop in obj) {
+                    if (self.models[name]._scheme[prop]) {
+                        toInsert[prop] = self.models[name]._scheme[prop].parse(obj[prop]);
+                    } else if (self.models[name]._nonStrict) {
+                        toInsert[prop] = obj[prop];
+                    }
+                }
+                for (let prop in toInsert) {
+                     // null == undefined
+                    if (toInsert[prop] == null && self.models[name]._scheme[prop] && self.models[name]._scheme[prop].notNull) {
+                        throw new Error(`Not null constraint failed for property '${prop}' of collection '${name}'.`);
+                    }
+                }
+                listToInsert.push(toInsert);
+            }
+            return self.models[name]._collection.insert(listToInsert);
+        }
+        this.models[name].insert = insert;
+
+        return this.models[name];
+    }
+}
+
+Jotnar.TYPES = _types;
+
+module.exports = Jotnar;
